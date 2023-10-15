069aaea2817b232396865965332aaf12fd1aa9d7
tjdud <tjdud@DESKTOP-7KJFUDJ>
2023-10-15 16:59:49 +0900

    과제

--- Project61/main.c
+++ Project61/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX_ELEMENT 200
+#define MAX_ELEMENT 100
 
 int count = 0;
 
@@ -20,51 +20,71 @@ HeapType* create() {
 void init(HeapType* h) {
     h->heap_size = 0;
 }
-void insert_max_heap(HeapType* h, element item) {
-    int i;
-    i = ++(h->heap_size);
 
-    while ((i != 1) && (item.key > h->heap[i / 2].key)) {
-        h->heap[i] = h->heap[i / 2];
-        i /= 2;
-        count++;
-    }
-    h->heap[i] = item;
-    print_heap(h); // 중간 상태 출력
-}
-
-element delete_max_heap(HeapType* h) {
-    int parent, child;
-    element item, temp;
-
-    item = h->heap[1];
-    temp = h->heap[(h->heap_size)--];
-    parent = 1;
-    child = 2;
-
-    while (child <= h->heap_size) {
-        if ((child < h->heap_size) && (h->heap[child].key < h->heap[child + 1].key)) {
-            child++;
-            count++;
-        }
-        if (temp.key >= h->heap[child].key) break;
-
-        h->heap[parent] = h->heap[child];
-        parent = child;
-        child *= 2;
-    }
-    h->heap[parent];
-    return item;
-}
-
-void print_heap(HeapType* h) {
+void print_heap(HeapType* h) { //과정 출력
     for (int i = 1; i <= h->heap_size; i++) {
         printf("%d ", h->heap[i].key);
     }
     printf("\n");
 }
 
-void print_level(HeapType* h) {
+void insert_max_heap(HeapType* h, element item) { //삽입
+    int i;
+    i = ++(h->heap_size);
+    h->heap[i] = item;
+    print_heap(h);
+    while ((i != 1) && (item.key > h->heap[i / 2].key)) {
+        h->heap[i] = h->heap[i / 2];
+        h->heap[i / 2] = item;
+        i /= 2;
+        count++;
+        print_heap(h); // 중간 상태 출력
+    }
+
+}
+void insert_first_heap(HeapType* h, element item) { //처음 삽입
+    int i;
+    i = ++(h->heap_size);
+
+    while ((i != 1) && (item.key > h->heap[i / 2].key)) {
+        h->heap[i] = h->heap[i / 2];
+        i /= 2;
+    }
+    h->heap[i] = item;
+}
+
+element delete_max_heap(HeapType* h) { //삭제
+    int parent, child;
+    element item;
+    print_heap(h);
+
+    item = h->heap[1]; //최상위 노드
+    h->heap[1] = h->heap[(h->heap_size)--]; //최상위 노드 삭제 -> 마지막 노드를 최상위 노드로 업데이트
+    parent = 1;
+    child = 2; //자식노드
+    print_heap(h);
+
+    while (child <= h->heap_size) {
+        count++;
+        if ((child < h->heap_size) && (h->heap[child].key < h->heap[child + 1].key)) {
+            child++;
+        } //왼쪽 자식보다 오른쪽 자식이 크면 자식 업데이트
+        if (h->heap[parent].key >= h->heap[child].key) break; //부모가 자식노드보다 크거나 같으면 break
+
+        element tmp = h->heap[parent]; //부모 노드 옮김
+        h->heap[parent] = h->heap[child]; //자식노드가 더 크면 자식 노드랑 부모 노드 위치 바꿈
+        h->heap[child] = tmp;
+        parent = child;
+        child *= 2;
+        print_heap(h);
+    }
+
+    return item;
+}
+
+
+
+void print_level(HeapType* h) { //레벨별 출력
     int level = 1;
     int count = 0;
     for (int i = 1; i <= h->heap_size; i++) {
@@ -86,7 +106,7 @@ int main(void) {
     for (int i = 0; i < size_element; i++) {
         element e;
         e.key = elements[i];
-        insert_max_heap(heap, e);
+        insert_first_heap(heap, e);
     }
 
     char choice;
@@ -100,12 +120,12 @@ int main(void) {
         printf("\n\n");
 
         printf("메뉴 입력 : ");
-        scanf(" %c", &choice);
+        scanf_s(" %c", &choice);
         int input;
 
         if (choice == 'i') {
             printf("추가할 값 입력 : ");
-            scanf("%d", &input);
+            scanf_s("%d", &input);
             element e;
             e.key = input;
             insert_max_heap(heap, e);
@@ -114,10 +134,6 @@ int main(void) {
             count = 0;
         }
         if (choice == 'd') {
-            printf("삭제할 값 입력 : ");
-            scanf("%d", &input);
-            element e;
-            e.key = input;
             delete_max_heap(heap);
             printf("노드가 이동한 횟수 : %d", count);
             count = 0;
@@ -130,8 +146,6 @@ int main(void) {
             printf("프로그램을 종료합니다.\n");
             break;
         }
-
         printf("\n");
     }
-    return 0;
 }
